0101dd3c49efd8d202b0ba824ce8d07215cf1082
mo-hassann <mohamed.hassan.bu@gmail.com>
2024-08-13 18:21:33 +0300

    update terminal UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="dark" lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="font-mono">{children}</body>
     </html>
   );
 }
--- components/layout/terminal.tsx
+++ components/layout/terminal.tsx
@@ -1,21 +1,35 @@
 "use client";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 import React, { useEffect, useRef, useState } from "react";
+import { TerminalInput } from "./terminal-input";
 
 export default function Terminal() {
   const [isFocused, setIsFocused] = useState(false);
-
+  const pathname = usePathname();
   const ref = useRef<null | HTMLUListElement>(null);
+  const [terminalLines, setTerminalLines] = useState<string[]>([`○ Compiling / ...`]);
+  const [inputValue, setInputValue] = useState("");
 
-  const scrollToBottom = () => {
-    ref.current?.lastElementChild?.scrollIntoView({ behavior: "instant" });
-  };
+  const inputRef = useRef<null | HTMLInputElement>(null);
+  const spanRef = useRef<null | HTMLSpanElement>(null);
 
+  useEffect(() => {
+    if (inputRef.current && spanRef.current) {
+      // Set the span text to the input value
+      spanRef.current.textContent = inputValue || " ";
+      // Calculate and set the width of the input based on the span
+      inputRef.current.style.width = `${spanRef.current.offsetWidth}px`;
+    }
+  }, [inputValue]);
+
+  // auto stack to the bottom
   useEffect(() => {
     if (!ref.current) return;
 
     const resizeObserver = new ResizeObserver(() => {
-      scrollToBottom();
+      // scroll to the bottom
+      ref.current?.lastElementChild?.scrollIntoView({ behavior: "instant" });
     });
 
     resizeObserver.observe(ref.current);
@@ -23,7 +37,12 @@ export default function Terminal() {
     return () => {
       resizeObserver.disconnect();
     };
-  }, []);
+  }, [pathname]);
+
+  // add elements every time the path name changes
+  useEffect(() => {
+    setTerminalLines((curState) => [...curState, `GET ${pathname} in ${Math.floor(Math.random() * 1000)}ms`, `✓ Compiled in ${(Math.random() * 1.5).toFixed(2)}s (${Math.floor(Math.random() * 1000)} modules)`]);
+  }, [pathname]);
 
   return (
     <div className="border-t w-full text-xs h-full bg-muted overflow-hidden">
@@ -39,54 +58,41 @@ export default function Terminal() {
         <li className="cursor-pointer hover:text-foreground">GITLENS</li>
       </ul>
       {/* console */}
-      <ul ref={ref} className="w-full px-6 pb-4 cursor-text h-full overflow-y-scroll" onClick={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}>
+      <ul ref={ref} className="w-full px-6 pt-8 pb-4 cursor-text h-full overflow-y-scroll" onClick={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}>
+        {terminalLines.map((line) => {
+          const lineArr = line.split("✓");
+          return (
+            <li key={Math.random()}>
+              {lineArr.map((value) =>
+                !value ? (
+                  <span className="text-green-500 font-extrabold" key={Math.random()}>
+                    ✓
+                  </span>
+                ) : (
+                  value
+                )
+              )}
+            </li>
+          );
+        })}
         <li>
-          <span className="font-bold">○</span> Compiling / ...
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled / in 5.1s (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 737ms (255 modules)
-        </li>
-        <li>
-          GET / <span className="font-bold text-green-600">200</span> in 5912ms
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 1690ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <span className="font-bold text-green-600">✓</span> Compiled in 429ms (538 modules)
-        </li>
-        <li>
-          <div className={cn("border w-1.5 h-4 border-foreground", isFocused && "bg-foreground")} />
+          {/* <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setTerminalLines((curValues) => [...curValues, inputValue]);
+              setInputValue("");
+            }}
+            className="w-full flex items-center"
+          >
+            <input onChange={(e) => setInputValue(e.target.value)} value={inputValue} type="text" ref={inputRef} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} className="bg-inherit h-auto outline-none w-4 text-foreground overflow-hidden resize-none" />
+            <div className={cn("border w-1.5 h-4 border-foreground", isFocused && "bg-foreground")} />
+          </form>
+          {/* Hidden span to measure the width of the text 
+          <span ref={spanRef} style={{ visibility: "hidden", whiteSpace: "pre", fontSize: "inherit", fontFamily: "inherit" }}>
+            {inputValue || " "}
+          </span>
+           */}
+          <TerminalInput />
         </li>
       </ul>
     </div>
